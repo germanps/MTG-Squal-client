3e9cdabfeafb946c85d62094b0983c36bf0caa71
German Pla <germanps@outlook.es>
2020-11-09 10:15:08 +0100

    auth page conditional

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container, Image } from 'semantic-ui-react'
+import logo from '../../assets/svg/header-logo.svg'
 import './Auth.scss';
 
 export default function Auth() {
+    const [showLogin, setShowLogin] = useState(false)
     return (
-        <div className="auth">
-            Auth
-        </div>
+        <Container fluid className="auth">
+            <Image src={logo} />
+            <div className="container-form">
+                {showLogin ? <p>Formulario de Login</p> : <p>Formulario de registro</p>}
+            </div>
+            <div className="change-form">
+                <p>
+                    {showLogin ? (
+                        <>
+                            no tienes cuenta?
+                            <span onClick={() => setShowLogin(!showLogin)}>Regístrate</span>
+                        </>
+                    ) : (
+                            <>
+                                Entra con tu cuenta
+                            <span onClick={() => setShowLogin(!showLogin)}>Iniciar sesión</span>
+                            </>
+                        )}
+                </p>
+            </div>
+        </Container>
     )
 }
